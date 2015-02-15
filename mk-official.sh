61b5b7dfb8e95d00db889fb22979805080048f81
Christopher Biscardi <biscarch@sketcht.com>
2015-02-15 08:30:40 -0800

    add dynamic git commit to official lists

--- mk-official.sh
+++ mk-official.sh
@@ -10,10 +10,12 @@ do
     name=$line
     if [ "$line" != "" ] && [[ $line != v0.5* ]]; then
         sed s/{folder}/"versions\/$line"/g official.template |
-            sed s/{raw-version}/${line#"v"}/g >> .tmp/official-commits
+            sed s/{raw-version}/${line#"v"}/g |
+            sed s/{commit}/$(git rev-parse HEAD)/g >> .tmp/official-commits
     fi
 done < .tmp/versions.txt
 
 LATEST=$(tail -1 .tmp/versions.txt)
 sed s/{folder}/"versions\/$LATEST"/g official.template |
-    sed s/{raw-version}/latest/g >> .tmp/official-commits
+    sed s/{raw-version}/latest/g |
+    sed s/{commit}/$(git rev-parse HEAD)/g >> .tmp/official-commits
